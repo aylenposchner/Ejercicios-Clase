07b6ca50d45d84e8a804fdc1995b2820651cb2e2
Aylen Poschner <aylenposchner@hotmail.com>
2024-12-08 19:59:16 -0300

    Str pais

--- src/clientes/models.py
+++ src/clientes/models.py
@@ -4,7 +4,7 @@ class Pais(models.Model):
     nombre = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
-        return f"{self.nombre}"
+        return self.nombre
 
 class Cliente(models.Model):
     nombre = models.CharField(max_length=255)
