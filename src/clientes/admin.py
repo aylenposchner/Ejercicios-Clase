a6598bbed29737b917a7fa845c4e0a1b0824a7d7
Aylen Poschner <aylenposchner@hotmail.com>
2024-12-09 08:06:34 -0300

    fin 08.12

--- src/clientes/admin.py
+++ src/clientes/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Cliente, Pais
 
-# Register your models here.
+# admin.site.register(Cliente)
+# admin.site.register(Pais)
+
+@admin.register(Pais)
+class PaisAdmin(admin.ModelAdmin):
+    list_display = ("nombre",)
+    search_fields = ("nombre",)
+    ordering = ("nombre",)
+
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ("nombre","apellido","nacimiento","pais_origen")
+    search_fields = ("nombre","apellido")
+    ordering = ("nombre","apellido")
+    list_filter = ("pais_origen",)
+    date_hierarchy = "nacimiento"
